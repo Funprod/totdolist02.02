6fa0399fca5787322e00f618c9a991bf00951723
Funprod <131615685+Funprod@users.noreply.github.com>
2024-08-19 18:16:46 +0300

    first commit

---
+++ src/Todolist.tsx
@@ -0,0 +1,38 @@
+import {TaskType} from "./App";
+import {Button} from "./Button";
+
+type PropsType = {
+	title: string
+	tasks: TaskType[]
+}
+
+export const Todolist = ({title, tasks}: PropsType) => {
+
+	return (
+		<div>
+			<h3>{title}</h3>
+			<div>
+				<input/>
+				<Button title={'+'}/>
+			</div>
+			{
+				tasks.length === 0
+					? <p>Тасок нет</p>
+					: <ul>
+						{tasks.map((task) => {
+							return(
+								<li key={task.id}><input type="checkbox" checked={task.isDone}/><span>{task.title}</span>
+								<button onClick={ () => { alert(task.id)}}>x</button>
+								</li>
+							)
+						})}
+					</ul>
+			}
+			<div>
+				<Button title={'All'}/>
+				<Button title={'Active'}/>
+				<Button title={'Completed'}/>
+			</div>
+		</div>
+	)
+}
